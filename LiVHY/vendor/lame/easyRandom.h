34b9e1b9c3e5474c1ac278e6e7e463bab970a2fd
Lame-een <43285057+Lame-een@users.noreply.github.com>
2022-09-15 11:59:18 +0200

    FIX random const function param

--- LiVHY/vendor/lame/easyRandom.h
+++ LiVHY/vendor/lame/easyRandom.h
@@ -60,12 +60,12 @@ namespace lameutil
 			std::random_device rd;
 			generator = std::default_random_engine(rd());
 		}
-		EasyRandom(const std::seed_seq& seed)
+		EasyRandom(std::seed_seq& seed)
 		{
 			setSeed(seed);
 		}
 
-		void setSeed(const std::seed_seq& seed)
+		void setSeed(std::seed_seq& seed)
 		{
 			generator.seed(seed);
 		}
